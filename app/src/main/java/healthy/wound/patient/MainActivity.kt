26154138e76fd0d229d2ac1409463209661fbb67
Tal Kanel <tal.kanel@healthy.io>
2023-02-19 09:53:58 +0200

    initial commit

---
+++ app/src/main/java/healthy/wound/patient/MainActivity.kt
@@ -0,0 +1,11 @@
+package healthy.wound.patient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
